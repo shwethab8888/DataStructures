788bef41efdd49fe085ae7d40cae2886b3db5029
shwethab8888 <shwethab8888@gmail.com>
2023-03-10 16:02:29 +0530

    Creating values in Queue using linked list

---
+++ QueueUsingLinkedList/CreateQueue.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueUsingLinkedList
+{
+    public class CreateQueue
+    {
+
+        private class Node
+        {
+            public int Value;
+            public Node Next;
+
+            public Node(int value)
+            {
+                Value = value;
+                Next = null;
+            }
+        }
+
+        private Node head;
+        private Node tail;
+
+        public void Enqueue(int value)
+        {
+            Node newNode = new Node(value);
+
+            if (tail == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                tail.Next = newNode;
+                tail = newNode;
+            }
+        }
+
+        public int Dequeue()
+        {
+            if (head == null)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            int value = head.Value;
+            head = head.Next;
+
+            if (head == null)
+            {
+                tail = null;
+            }
+
+            return value;
+        }
+
+        public int Peek()
+        {
+            if (head == null)
+            {
+                throw new InvalidOperationException("Queue is empty");
+            }
+
+            return head.Value;
+        }
+
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+        public static void Create_Queue()
+        {
+            CreateQueue myQueue = new CreateQueue();
+            myQueue.Enqueue(56);
+            myQueue.Enqueue(30);
+            myQueue.Enqueue(70);
+
+            while (!myQueue.IsEmpty())
+            {
+                int value = myQueue.Dequeue();
+                Console.WriteLine(value);
+            }
+        }
+
+
+    }
+}
---
+++ QueueUsingLinkedList/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace QueueUsingLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome to Queue using Linked List");
+
+
+            CreateQueue.Create_Queue();
+
+        }
+    }
+}
