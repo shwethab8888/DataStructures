5c0c4c43141664be5016567fb808d0295d86e121
shwethab8888 <shwethab8888@gmail.com>
2023-03-10 20:38:37 +0530

    Create Linked list values

---
+++ LinkedList/CreateList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+
+    public class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+
+    public class CreateList
+    {
+        Node head;
+
+        public void Add(int data)
+        {
+            Node new_node = new Node(data);
+
+            if (head == null)
+            {
+                head = new_node;
+                return;
+            }
+
+            Node last_node = head;
+            while (last_node.next != null)
+            {
+                last_node = last_node.next;
+            }
+
+            last_node.next = new_node;
+        }
+
+        public void PrintList()
+        {
+            Node current_node = head;
+            while (current_node != null)
+            {
+                Console.Write(current_node.data + " ");
+                current_node = current_node.next;
+            }
+            Console.WriteLine();
+        }
+    
+
+        public static void Create_List()
+        {
+            CreateList linked_list = new CreateList();
+
+            // Add elements
+            linked_list.Add(56);
+            linked_list.Add(30);
+            linked_list.Add(70);
+
+            // Print the linked list
+            linked_list.PrintList();
+        }
+    }
+
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace LinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Welcome to LinkedList");
+
+            CreateList.Create_List();
+
+        
+        }
+    }
+}
